fa372b6b85928ae84068f170cb45af46a974f134
Ramon Bosi <ramombosi2015@gmail.com>
2022-02-06 17:06:48 -0300

    visual do menu melhorado

--- site_noticia/site_noticia_JS.js
+++ site_noticia/site_noticia_JS.js
@@ -1,13 +1,15 @@
 
-const popup_conteudo_navegacao = document.querySelector('#popup_conteudo_navegacao')
+const fundo_popup_conteudo_navegacao = document.querySelector('#fundo_popup_conteudo_navegacao')
+const corpo_principal = document.querySelector('body')
 
 const classes_css = {
-    exibir_elemento: 'ocultar'
+    exibir_elemento: 'ocultar',
+    ocultar_overflow: 'ocultar_overflow'
 }
 
 function mostrar_popup(popup_selecionado){
     
-   const classes_ativas_popup_conteudo_navegacao = popup_conteudo_navegacao.classList
+   const classes_ativas_fundo_popup_conteudo_navegacao = fundo_popup_conteudo_navegacao.classList
    const popup_mostrado =  document.querySelector(`#${popup_selecionado}`)
    const todos_popup_navegacao = document.querySelectorAll('#popup_noticias, #popup_comparar, #popup_cupons_descontos')
    
@@ -15,12 +17,14 @@ function mostrar_popup(popup_selecionado){
         e.classList.add(classes_css.exibir_elemento)
     })
 
-    classes_ativas_popup_conteudo_navegacao.remove(classes_css.exibir_elemento)
+    corpo_principal.classList.add(classes_css['ocultar_overflow'])
     popup_mostrado.classList.remove(classes_css.exibir_elemento)
+    classes_ativas_fundo_popup_conteudo_navegacao.remove(classes_css.exibir_elemento)
 }
 
-function fechar_popup(){
-    popup_conteudo_navegacao.classList.add(classes_css.exibir_elemento)
+function fechar_fundo_popup_conteudo_navegacao(){
+    corpo_principal.classList.remove(classes_css['ocultar_overflow'])
+    fundo_popup_conteudo_navegacao.classList.add(classes_css['exibir_elemento'])
 }
 
 let exibir = true
